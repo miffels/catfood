6110383fe5d8c16691ca20c9cdfdb4c5117b50ef
Michael Jess <michael.jess@gmx.de>
2015-07-13 10:43:57 +0200

    Adding null domainClass example

--- gorm-mongodb-statictest/src/integration-test/groovy/gorm/mongodb/statictest/LanguageFunctionalSpec.groovy
+++ gorm-mongodb-statictest/src/integration-test/groovy/gorm/mongodb/statictest/LanguageFunctionalSpec.groovy
@@ -6,12 +6,24 @@ import grails.test.mixin.TestFor
 import grails.test.mixin.integration.Integration
 import grails.transaction.*
 
+import org.junit.ClassRule
+import org.junit.Rule
+
 import spock.lang.*
+
+import com.eltepi.test.mongodb.EmbedMongoRule
+import com.eltepi.test.mongodb.ResetDatabaseRule
 
 
 @Integration(applicationClass=Application)
 @TestFor(Language)
 class LanguageFunctionalSpec extends Specification {
+	
+	@Shared @ClassRule
+	EmbedMongoRule embedMongoRule = new EmbedMongoRule(grailsApplication)
+	
+	@Rule
+	public ResetDatabaseRule resetDatabaseRule = new ResetDatabaseRule(embedMongoRule.db)
 	
     void "Test initialization"() {
         expect: "Default languages should be there"
